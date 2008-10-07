9650d7f11901d4276fd8e507d37b99c1b7f6f4af
Adrian Kuhn <akuhn@iam.unibe.ch>
2008-10-07 23:01:48 +0000

    Reading default value if none set. Included shitload of assertions. Crappy but functional icons.

--- src/ch/unibe/iam/scg/fontsizebuttons/actions/ChangeFontSize.java
+++ src/ch/unibe/iam/scg/fontsizebuttons/actions/ChangeFontSize.java
@@ -1,5 +1,8 @@
 package ch.unibe.iam.scg.fontsizebuttons.actions;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.preferences.IPreferencesService;
 import org.eclipse.jface.action.IAction;
@@ -19,18 +22,31 @@ public abstract class ChangeFontSize implements IWorkbenchWindowActionDelegate {
 	public void run(IAction action) {
 		try { run(); }
 		catch (Exception ex) {
+			if (ex instanceof RuntimeException) throw (RuntimeException) ex;
 			throw new RuntimeException(ex);
 		}
 	}
 	
 	public void run() throws Exception {
 		IPreferencesService r = Platform.getPreferencesService();
-		Preferences n = r.getRootNode().node("/instance/org.eclipse.ui.workbench");
-		String value = n.get("org.eclipse.jface.textfont", null);
-		FontData fontdata = PreferenceConverter.basicGetFontData(value)[0];
+		String value = r.getString("org.eclipse.ui.workbench",
+		        "org.eclipse.jface.textfont", 
+		        null, null);
+		if (value == null) throw new AssertionError();
+		FontData[] ary = PreferenceConverter.basicGetFontData(value);
+		if (ary == null) throw new AssertionError();
+		if (ary.length < 1) throw new AssertionError();
+        FontData fontdata = ary[0];
 		fontdata.setHeight(changeFontSize(fontdata.getHeight()));
+        Preferences n = r.getRootNode().node("/instance/org.eclipse.ui.workbench");
 		n.put("org.eclipse.jface.textfont", fontdata.toString());
 		n.flush();
+	}
+	
+	protected void debugPreferencesService(IPreferencesService r) throws Exception {
+	    OutputStream baos = new ByteArrayOutputStream();
+	    r.exportPreferences(r.getRootNode(), baos, null);
+	    System.out.println(baos);
 	}
 	
 	protected abstract int changeFontSize(int height);
